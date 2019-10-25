ab20ec3220e0e17d5876a268b3859a92786ee992
Nikita Volobuev <nikitavbv@gmail.com>
2019-10-25 12:03:32 +0300

    feat(loggers): list loggers

--- frontend/src/loggers/Loggers.tsx
+++ frontend/src/loggers/Loggers.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+type Logger = {
+    name: string,
+    id: string
+}
+
 type LoggersState = {
     logger_name_input: string,
-    created_logger_id: string | undefined,  
+    created_logger_id: string | undefined,
+    loggers: Logger[],
 };
 
 export class Loggers extends React.Component {
@@ -12,7 +18,11 @@ export class Loggers extends React.Component {
         super({});
         this.state = {
             logger_name_input: '',
+            created_logger_id: undefined,
+            loggers: [],
         } as LoggersState;
+
+        this.request_loggers();
     }
 
     render() {
@@ -22,8 +32,23 @@ export class Loggers extends React.Component {
                 { this.render_created_logger_message() }
                 <input placeholder="Logger name" value={this.state.logger_name_input} onChange={this.handle_logger_name_change.bind(this)} />
                 <button onClick={this.create_logger.bind(this)}>Create</button>
+
+                <h1>Loggers</h1>
+                { this.render_loggers_list(this.state.loggers) }
             </div>
         );
+    }
+
+    request_loggers() {
+        fetch('/api/v1/logger', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(res => res.json())
+            .then(data => this.setState({ ...this.state, loggers: data.loggers }));
     }
 
     render_created_logger_message() {
@@ -39,6 +64,7 @@ export class Loggers extends React.Component {
     }
 
     create_logger() {
+        const name = this.state.logger_name_input;
         this.setState({ ...this.state, logger_name_input: '' });
 
         fetch('/api/v1/logger', {
@@ -48,10 +74,34 @@ export class Loggers extends React.Component {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                name: this.state.logger_name_input
+                name,
             })
         }).then(res => res.json().then(data => {
-            this.setState({ ...this.state, created_logger_id: data.logger_id });
+            this.setState({ ...this.state, created_logger_id: data.logger_id, loggers: [...this.state.loggers, {
+                name,
+                id: data.logger_id, 
+            }] });
         }));
+    }
+
+    render_loggers_list(loggers: Logger[]) {
+        return (
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>API Key</th>
+                </tr>
+                { loggers.map(logger => this.render_loggers_list_row(logger)) }
+            </table>
+        )
+    }
+
+    render_loggers_list_row(logger: Logger) {
+        return (
+            <tr>
+                <td>{ logger.name }</td>
+                <td>{ logger.id }</td>
+            </tr>
+        )
     }
 }
--- src/logger.ts
+++ src/logger.ts
@@ -7,6 +7,19 @@ import { HttpStream, HttpMethod, HttpRequest } from "./api";
 
 const logging_context = {};
 
+const get_loggers = async (database: Client, req: HttpRequest) => {
+    if (req.auth === undefined) {
+        throw new AssertionError({ message: 'auth is expected'});
+    }
+
+    const user_id = req.auth.user_id;
+    const result = await database.query('SELECT id, name FROM loggers WHERE user_id = $1', [ user_id ]);
+
+    req.ok({
+        loggers: result.rows,
+    });
+};
+
 const create_logger = async (database: Client, req: HttpRequest) => {
     if (req.auth === undefined) {
         throw new AssertionError({ message: 'auth is expected' });
@@ -25,5 +38,6 @@ const create_logger = async (database: Client, req: HttpRequest) => {
 };
 
 export default (stream: HttpStream, database: Client) => {
+    stream.url('/').method(HttpMethod.GET).forEach(get_loggers.bind(logging_context, database));
     stream.url('/').method(HttpMethod.POST).forEach(create_logger.bind(logging_context, database));
 };
