d7ff205b4f633fcaec6ef9c50e937cf7dd60db60
Nikita Volobuev <nikitavbv@gmail.com>
2019-11-05 23:39:08 +0200

    feat(examples): sequential

--- async_iterator_examples.js
+++ async_iterator_examples.js
@@ -3,3 +3,6 @@ const metasync = require('metasync');
 const x = metasync.asyncIter([1, 2, 3, 4, 5])
     .map(x => x * 2)
     .forEach(console.log);
+
+const f = metasync([(x, y) => y(null, { b: x.n*2 }), (x, y) => y(null, { a: x.b+1 })]);
+f({ n: 42 }, console.log);
