377419d8a02ac80316c031fb24823a8907d26efc
Nikita Volobuev <nikitavbv@gmail.com>
2019-11-06 23:21:19 +0200

    fix(examples): use same name in chain

--- async_iterator_examples.js
+++ async_iterator_examples.js
@@ -4,5 +4,5 @@ const x = metasync.asyncIter([1, 2, 3, 4, 5])
     .map(x => x * 2)
     .forEach(console.log);
 
-const f = metasync([(x, y) => y(null, { b: x.n*2 }), (x, y) => y(null, { a: x.b+1 })]);
-f({ n: 42 }, console.log);
+const f = metasync([(x, y) => y(null, { arg: x.arg * 2 }), (x, y) => y(null, { arg: x.arg + 1 })]);
+f({ arg: 42 }, console.log);
