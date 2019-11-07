518b5651c6e2e609e846d6f76cb649c350b28e6b
Nikita Volobuev <nikitavbv@gmail.com>
2019-11-07 23:17:14 +0200

    feat(examples): add async iterator example

--- async_iterator_examples.js
+++ async_iterator_examples.js
@@ -2,7 +2,23 @@ const metasync = require('metasync');
 
 const x = metasync.asyncIter([1, 2, 3, 4, 5])
     .map(x => x * 2)
-    .forEach(console.log);
+//    .forEach(console.log);
 
 const f = metasync([(x, y) => y(null, { arg: x.arg * 2 }), (x, y) => y(null, { arg: x.arg + 1 })]);
 f({ arg: 42 }, console.log);
+
+const asyncIterable = {
+    [Symbol.asyncIterator]: async function* asyncGenerator() {
+        const array = [0, 1, 2, 3, 4];
+    
+        while (array.length > 0) {
+            yield await Promise.resolve(array.shift());
+        }
+    }
+};
+  
+const asyncIterResult = metasync.asyncIter(asyncIterable)
+    .map(x => x * 2)
+    .reduce((a, b) => a + b);
+
+asyncIterResult.then(console.log);
