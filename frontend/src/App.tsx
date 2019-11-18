ad3cf21feb327a231243cc7ee881965417d3ac10
Nikita Volobuev <nikitavbv@gmail.com>
2019-11-18 22:23:09 +0200

    feat(frontend): move header to a separate component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { Home } from './home';
 import { Auth, loadGapi } from './auth';
 import { Loggers } from './loggers';
 
+import { Header } from './components';
+
 import './App.css';
 
 const is_authenticated = () => localStorage.getItem('authenticated') != null;
@@ -21,48 +23,15 @@ const require_auth = (nextState: RouterState, replace: Function, next: Function)
   next();
 }
 
-const log_out = (app: App) => {
-  localStorage.removeItem('authenticated');
-  fetch('/api/v1/auth/logout', {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({})
-  }).then(() => {
-    window.location.href = '/home';
-    app.setState({});
-  });
-};
 
 export class App extends React.Component {
 
   render() {
-    const header_links_requiring_auth = is_authenticated() ? (
-      <div className="user-links">
-        <a href="/loggers">Loggers</a>
-        <button onClick={log_out.bind(this, this)}>Log out</button>
-      </div>
-    ) : (
-      <div className="user-links">
-        <a href="/auth">Auth</a>
-      </div>
-    );
-  
     return (
       <Router>
           <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,300,400" rel="stylesheet" />
           <div className="app">
-            <header className="header">
-              <a href="/">Logging</a>
-  
-              <div className="header-ctl">
-                <div className="links">
-                  { header_links_requiring_auth }
-                </div>
-              </div>
-            </header>
+            <Header />
             <main className="content">
               <Route path="/" exact component={Home} />
               <Route path="/auth" render={() => <Auth loadGapi={loadGapi} />} />
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const is_authenticated = () => localStorage.getItem('authenticated') != null;
+
+const log_out = (component: React.Component) => {
+    localStorage.removeItem('authenticated');
+    fetch('/api/v1/auth/logout', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
+    }).then(() => {
+        window.location.href = '/home';
+        component.setState({});
+    });
+};
+
+export class Header extends React.Component {
+
+    render() {
+        const header_links_requiring_auth = is_authenticated() ? (
+            <div className="user-links">
+                <a href="/loggers">Loggers</a>
+                <button onClick={log_out.bind(this, this)}>Log out</button>
+            </div>
+        ) : (
+            <div className="user-links">
+                <a href="/auth">Auth</a>
+            </div>
+        );
+
+
+        return (
+            <header className="header">
+                <a href="/">Logging</a>
+
+                <div className="header-ctl">
+                    <div className="links">
+                        {header_links_requiring_auth}
+                    </div>
+                </div>
+            </header>
+        );
+    }
+}
